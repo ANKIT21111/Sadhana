9fc1c9797ce21e422dd45d6565b0d63f60dd4b2e
ANKIT21111 <ankitabhishek1005@gmail.com>
2026-02-05 15:08:05 +0530

    feat: Add Android platform to the Capacitor project.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sadhana.app',
+  appName: 'Sadhana',
+  webDir: 'dist'
+};
+
+export default config;
